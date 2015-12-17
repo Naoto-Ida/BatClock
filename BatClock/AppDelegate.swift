b8be177ffbe6b13f4fa9ef0b3e2a44b6cbf38257
Naoto Ida <naoto.ida+programming@gmail.com>
2015-12-17 22:00:45 +0900

    first commit

---
+++ BatClock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BatClock
+//
+//  Created by Naoto Ida on 12/17/15.
+//  Copyright © 2015 naotoida. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
